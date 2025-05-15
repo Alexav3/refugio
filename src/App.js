3bcadfce4a7278f72cf2f355f9d38bea431e1e49
Your Name <you@example.com>
2025-05-14 19:49:01 -0500

    creando_rutas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
+// App.js
 import React from "react";
-import "./App.css";
-import Navbar from "./Navbar";
-import Footer from "./footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="app-container">
-      <Navbar />
-      <h1>El Nuevo Refugio, Jalisco, México.</h1>
-      <h3>Bienvenidos a la comunidad de El Nuevo Refugio, Jalisco.</h3>
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <h1>El Nuevo Refugio, Jalisco, México.</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
