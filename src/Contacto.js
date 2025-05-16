e7aedcc9f73e3ecdaeafd7d46686d6ef9e9cb2c4
Your Name <you@example.com>
2025-05-16 10:25:52 -0500

    color_design

--- src/Contacto.js
+++ src/Contacto.js
@@ -1,11 +1,26 @@
 // src/Contacto.js
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import "./Contacto.css";
 
 export default function Contacto() {
   return (
-    <div>
-      <h1>Contactanos</h1>
-      <p>En esta pagina formas de contacto con la administracion</p>
+    <div className="contact-container">
+      <h1 className="contact-title">Contáctanos</h1>
+      <p className="contact-text">Encuentranos en: </p>
+      <ul className="contact-list">
+        <li>
+          <a
+            href="https://www.facebook.com/NuevoRefugioJalisco"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact-link"
+          >
+            <FaFacebook className="facebook-icon" />
+            Facebook
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,10 @@
 import React from "react";
 import "./Home.css";
-import plaza from "./images/plaza.jpg";
-import hacienda from "./images/hacienda.jpg";
 
 function Home() {
-  const images = [plaza, hacienda];
-  const [current, setCurrent] = React.useState(0);
-
-  const nextSlide = () => {
-    setCurrent((prev) => (prev + 1) % images.length);
-  };
-
-  const prevSlide = () => {
-    setCurrent((prev) => (prev - 1 + images.length) % images.length);
-  };
-
   return (
     <div className="home-container">
       <h1>El Nuevo Refugio, Jalisco, México.</h1>
-      <div className="slider">
-        <button onClick={prevSlide}>⟨</button>
-        <img src={images[current]} alt={`Foto ${current + 1}`} />
-        <button onClick={nextSlide}>⟩</button>
-      </div>
     </div>
   );
 }
