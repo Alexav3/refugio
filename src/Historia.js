19c42d0e524cb947ff2ebd700b5950bee0a26581
Your Name <you@example.com>
2025-05-19 17:53:37 -0500

    historia

--- src/Historia.js
+++ src/Historia.js
@@ -7,7 +7,50 @@ export default function Historia() {
   return (
     <div>
       <h1>Historia del rancho</h1>
-      <p>En esta pagina incluiremos nuestra historia</p>
+      <p>
+        El Nuevo Refugio es una pequeña comunidad localizada en la región
+        Ciénega. Su cabecera municipal es Tototlán y está localizada en el
+        estado de Jalisco, México. De acuerdo con cifras del IIEG (Instituto de
+        Información Estadística y Geográfica) en el año 2020, la localidad de El
+        Nuevo Refugio contaba con una población de 1,106 habitantes, de los
+        cuales el 46.8% eran hombres y el 53.2% mujeres. La población de 18 años
+        y más es de 733 personas, lo que representa el 66.3% de la población
+        total, de las cuales el 45.4% corresponde a hombres y el 54.6% a
+        mujeres.
+      </p>
+
+      <p>
+        El 3.3% de la población de 15 años y más es analfabeta, y el 4.2% no
+        cuenta con escolaridad. El 12.5% no tiene la primaria completa, y el
+        2.4% no cuenta con la secundaria completa. En los casos anteriores, las
+        mujeres son quienes presentan la mayor proporción de primaria completa y
+        secundaria completa, mientras que los hombres presentan una mayor
+        proporción de analfabetismo y de escolaridad completa.
+      </p>
+
+      <p>
+        En lo que se refiere a la población de 18 años y más, el 18.8% cuenta
+        con educación posbásica y el promedio de años estudiados en esta
+        localidad es de 7.5.
+      </p>
+
+      <p>
+        El 54.9% de la población de 12 años y más es económicamente activa; de
+        ella, el 98.1% corresponde a población ocupada. En ambos casos, la
+        mayoría es población masculina.
+      </p>
+
+      <p>
+        El 66.5% de la población es derechohabiente, es decir, 736 personas
+        tienen acceso a servicios de salud con la siguiente distribución: 248
+        personas al IMSS, 17 al ISSSTE, 7 al ISSSTE estatal y 456 al Seguro
+        Popular o Seguro Médico para una Nueva Generación.
+      </p>
+
+      <p>
+        El 29.2% de la población de 12 años y más es soltera o nunca se ha
+        casado.
+      </p>
     </div>
   );
 }
