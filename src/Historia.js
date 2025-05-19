f8b5c4c3b90928f1d0303a70d31284f02e926acf
Your Name <you@example.com>
2025-05-19 18:15:02 -0500

    historia_completa

--- src/Historia.js
+++ src/Historia.js
@@ -1,18 +1,17 @@
 // src/Historia.js
 import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./footer";
+import "./Historia.css";
 
 export default function Historia() {
   return (
-    <div>
-      <h1>Historia del rancho</h1>
+    <div className="historia">
+      <h1>Historia y Estadística.</h1>
       <p>
         El Nuevo Refugio es una pequeña comunidad localizada en la región
         Ciénega. Su cabecera municipal es Tototlán y está localizada en el
         estado de Jalisco, México. De acuerdo con cifras del IIEG (Instituto de
-        Información Estadística y Geográfica) en el año 2020, la localidad de El
-        Nuevo Refugio contaba con una población de 1,106 habitantes, de los
+        Información Estadística y Geográfica), en el año 2020, la localidad de
+        El Nuevo Refugio contaba con una población de 1,106 habitantes, de los
         cuales el 46.8% eran hombres y el 53.2% mujeres. La población de 18 años
         y más es de 733 personas, lo que representa el 66.3% de la población
         total, de las cuales el 45.4% corresponde a hombres y el 54.6% a
@@ -44,12 +43,30 @@ export default function Historia() {
         El 66.5% de la población es derechohabiente, es decir, 736 personas
         tienen acceso a servicios de salud con la siguiente distribución: 248
         personas al IMSS, 17 al ISSSTE, 7 al ISSSTE estatal y 456 al Seguro
-        Popular o Seguro Médico para una Nueva Generación.
+        Popular o Seguro Médico para una Nueva Generación. El 29.2% de la
+        población de 12 años y más es soltera o nunca estuvo casada, el 58% es
+        casada o unida, y el 12.8% estuvo casada o unida.
       </p>
 
       <p>
-        El 29.2% de la población de 12 años y más es soltera o nunca se ha
-        casado.
+        La comunidad de El Nuevo Refugio fue fundada por ejidatarios de la
+        región, quienes se establecieron en esta zona en busca de mejores
+        oportunidades de vida. A lo largo de los años, la comunidad ha crecido y
+        se ha desarrollado, convirtiéndose en un lugar próspero y lleno de vida.
+        Cuenta con una rica historia cultural y tradiciones que han sido
+        transmitidas de generación en generación. Por ejemplo, la celebración de
+        Navidad, donde se le canta al Niño Dios y se hacen posadas, es una
+        tradición única de la comunidad.
+      </p>
+
+      <p>
+        La población de El Nuevo Refugio es predominantemente católica y
+        pertenece a la Diócesis de San Juan de los Lagos. Como parte de esta
+        tradición religiosa, cada mes de enero se realiza una peregrinación en
+        honor a la Virgen de San Juan, una celebración que reúne a numerosos
+        fieles de la comunidad y sus alrededores. Esta manifestación de fe forma
+        parte de las costumbres más arraigadas del pueblo y refuerza los lazos
+        de identidad religiosa y cultural entre sus habitantes.
       </p>
     </div>
   );
